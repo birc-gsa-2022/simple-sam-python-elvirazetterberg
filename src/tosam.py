383e05221b3017b74ca3ca6a9fed793e8037a8cd
elvirazetterberg <elvira.zetterberg@gmail.com>
2022-09-10 11:55:11 +0200

    Update for project

--- src/tosam.py
+++ src/tosam.py
@@ -1,10 +1,5 @@
 import argparse
 
-# HELP PLEASE! I don't understand what format this is supposed to be outputted in.
-# From the test-tosam.sh file i gather that the result will be compared to 
-# mississippi.sam. The sam-file has an extra column with the CIGAR which isn't
-# available in mississippi.mas. I'm not sure what to do.
-
 def main():
     argparser = argparse.ArgumentParser(description="To Simple-SAM converter")
     argparser.add_argument(
@@ -12,13 +7,12 @@ def main():
         type=argparse.FileType('r')
     )
     args = argparser.parse_args()
-    # print(args)
+
     for line in args.mas:
         chrom, read_name, read_str, pos = line.split('\t')
-
-        print(read_name.strip(), chrom.strip(), str(int(pos)+1), f"{len(read_str.strip())}M", read_str.strip(), sep = '\t')
+        
         # Output as Simple-SAM
-
+        print(read_name.strip(), chrom.strip(), str(int(pos)+1), f"{len(read_str.strip())}M", read_str.strip(), sep = '\t')
 
 if __name__ == '__main__':
     main()
